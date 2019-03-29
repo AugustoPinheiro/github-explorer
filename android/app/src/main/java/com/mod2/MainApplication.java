e5add7ac9588c71c8b6bd9f394abc1414788cadb
Diego Fernandes <diego.schell.f@gmail.com>
2019-03-29 13:31:20 -0300

    Configurações de Push Notification

--- android/app/src/main/java/com/mod2/MainApplication.java
+++ android/app/src/main/java/com/mod2/MainApplication.java
@@ -3,6 +3,7 @@ package com.mod2;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.microsoft.codepush.react.CodePush;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -32,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeOneSignalPackage(),
           new CodePush(BuildConfig.CODEPUSH_KEY, getApplicationContext(), BuildConfig.DEBUG),
           new VectorIconsPackage(),
           new RNGestureHandlerPackage()
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { AsyncStorage } from 'react-native';
 import CodePush from 'react-native-code-push';
+import OneSignal from 'react-native-onesignal';
 import '~/config/ReactotronConfig';
 
 import createNavigator from './routes';
@@ -12,12 +13,33 @@ class App extends Component {
   };
 
   async componentDidMount() {
+    OneSignal.init('08071658-e65f-4aa9-a9a9-f92c2e2c9a26');
+
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+
     const username = await AsyncStorage.getItem('@Githuber:username');
 
     this.setState({
       userChecked: true,
       userLogged: !!username,
     });
+  }
+
+  onReceived = (notification) => {
+    console.log('Notification received: ', notification);
+  };
+
+  onOpened = (openResult) => {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  };
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
   }
 
   render() {
